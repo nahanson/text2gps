945810780f1aabef3d12c3c75ce19bf619c1e444
james.a.young <james.a.young@6737ef3c-a90f-2ba8-9ba7-cfc9621fb683>
2010-06-15 00:47:53 +0000

    tag version 1.1.2

---
+++ tags/version-1-1-2/src/uk/co/jamesy/Text2GPS/FinderReceiver.java
@@ -0,0 +1,61 @@
+package uk.co.jamesy.Text2GPS;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
+import uk.co.jamesy.Text2GPS.PhoneFinder;
+
+//this is to stop eclipse complaining 
+//(smsmessage is depreciated since 1.6 but as this app is technically 1.5 we will have to live with it)
+@SuppressWarnings("deprecation") 
+public class FinderReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String from = "";
+		String message = "";
+
+		SharedPreferences passwdfile = context.getSharedPreferences(
+				PhoneFinder.PASSWORD_PREF_KEY, 0);
+
+		String correctMd5 = passwdfile.getString(PhoneFinder.PASSWORD_PREF_KEY,
+				null);
+
+		// get the calling message
+		Bundle bundle = intent.getExtras();
+		SmsMessage[] msgs = null;
+
+		if (bundle != null) {
+			// retrieve the message
+			Object[] pdus = (Object[]) bundle.get("pdus");
+
+			msgs = new SmsMessage[pdus.length];
+
+			for (int i = 0; i < msgs.length; i++) {
+				msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+				from = msgs[i].getOriginatingAddress();
+				message = msgs[i].getMessageBody().toString();
+			}
+
+			// check password is set and message contains activation key
+			if (correctMd5 != null
+					&& message.contains(context.getResources().getString(
+							(R.string.key)))) {
+
+				// store message details and start the location service
+				Intent i = new Intent(context, FindResponse.class);
+				Bundle b = new Bundle();
+
+				b.putString("FROM", from);
+				b.putString("MESSAGE", message);
+				b.putString("MD5", correctMd5);
+				i.putExtras(b);
+
+				context.startService(i);
+			}
+		}
+	}
+}
